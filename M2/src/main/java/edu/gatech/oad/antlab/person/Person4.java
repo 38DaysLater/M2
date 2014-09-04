15d017f7758db1e15f5e7995832f1cdae4f14183
Ramvling <ramvling@gmail.com>
2014-09-03 21:18:04 -0400

    try 2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -37,6 +37,7 @@ public class Person4 {
             current ++;
             output = output + current;
             //this is a test. my name is not added properly to github
+            //test 2
 
         }
       return output;
